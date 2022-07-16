6f2c6fae3b248b0758d00d87d89baa57a85d5573
GuilhermeGz <guilherme.dna06@gmail.com>
2022-07-16 20:24:36 -0300

    Criada entidade Operacao

---
+++ database/migrations/2022_07_16_213019_create_operacaos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operacaos', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('observacao_recuperacao');
+            $table->boolean('outros_cuidados_recuperacao');
+            $table->boolean('analgesia_recuperacao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operacaos');
+    }
+};
