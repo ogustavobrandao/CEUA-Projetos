7747a611bc654b971fc8ebf65cadab5651928658
GuilhermeGz <guilherme.dna06@gmail.com>
2022-07-16 20:26:16 -0300

    Criada entidade Perfil

---
+++ database/migrations/2022_07_16_224523_create_perfils_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfils', function (Blueprint $table) {
+            $table->id();
+            $table->string('grupo_animal');
+            $table->string('linhagem');
+            $table->string('idade');
+            $table->string('peso');
+            $table->string('quantidade');
+            $table->string('machos');
+            $table->string('femeas');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfils');
+    }
+};
