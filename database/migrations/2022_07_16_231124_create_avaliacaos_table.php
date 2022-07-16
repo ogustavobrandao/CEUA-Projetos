4dde19ae873280cdde82328673c31695344a09f5
GuilhermeGz <guilherme.dna06@gmail.com>
2022-07-16 20:27:46 -0300

    Criada entidade Avaliacao

---
+++ database/migrations/2022_07_16_231124_create_avaliacaos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avaliacaos', function (Blueprint $table) {
+            $table->id();
+            $table->text('parecer');
+            $table->string('status');
+            $table->unsignedBigInteger('usuario_id');
+            $table->unsignedBigInteger('solicitacao_id');
+
+            $table->foreign('usuario_id')->references('id')->on('users');
+            $table->foreign('solicitacao_id')->references('id')->on('solicitacaos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avaliacaos');
+    }
+};
