206528f9fd0839ae3f406acd335f49b68489b4bb
GuilhermeGz <guilherme.dna06@gmail.com>
2022-07-16 20:25:31 -0300

    Criada entidade Planejamento

---
+++ database/migrations/2022_07_16_214859_create_planejamentos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planejamentos', function (Blueprint $table) {
+            $table->id();
+            $table->text('grau_invasividade');
+            $table->string('anexo_formula')->nullable();
+            $table->string('justificativa')->nullable();
+            $table->text('outras_infos');
+            $table->text('analise_estatistica');
+            $table->text('especificar_grupo');
+            $table->text('criterios');
+            $table->text('desc_materiais_metodos');
+            $table->string('num_animais_grupo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planejamentos');
+    }
+};
