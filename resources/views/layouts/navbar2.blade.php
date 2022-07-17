458fbca483d528f2731682376506eab14dcce865
Edgarvital <edgar.tecnico@hotmail.com>
2022-07-16 22:08:22 -0300

    Ajuste no navbar

--- resources/views/layouts/navbar2.blade.php
+++ resources/views/layouts/navbar2.blade.php
@@ -19,25 +19,25 @@
             @guest
                 @if (Route::has('login'))
                     <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('Sobre') }}</a>
+                        <a class="nav-link" href="#" style="color: black;">{{ __('Sobre') }}</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('Contato') }}</a>
+                        <a class="nav-link" href="#" style="color: black;">{{ __('Contato') }}</a>
                     </li>
                 @endif
 
 
             @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v- style="color: black">
+                        {{ 'Olá,'. Auth::user()->name }}
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
+                            {{ __('Sair') }}
                         </a>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
