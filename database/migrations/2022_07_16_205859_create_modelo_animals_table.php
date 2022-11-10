bf6df92ad1fcb89faa3aa2d745e2ca138d556d78
GuilhermeGz <guilherme.dna06@gmail.com>
2022-11-10 15:19:45 -0300

    Alterado da nomeção no migrate do termo de compromisso utilizado no modelo animal

--- database/migrations/2022_07_16_205859_create_modelo_animals_table.php
+++ database/migrations/2022_07_16_205859_create_modelo_animals_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('modelo_animals', function (Blueprint $table) {
             $table->id();
             $table->string('nome_vulgar');
-            $table->string('tcle');
+            $table->string('termo_consentimento');
             $table->string('nome_cientifico');
             $table->text('justificativa');
             $table->boolean('geneticamente_modificado');
