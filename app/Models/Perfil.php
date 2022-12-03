c82542b6154128d80dfad09857d806a753767f87
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-03 15:37:40 -0300

    Validações de Perfil

--- app/Models/Perfil.php
+++ app/Models/Perfil.php
@@ -21,6 +21,24 @@ class Perfil extends Model
         'modelo_animal_id',
     ];
 
+    public static $rules = [
+        'peso' => 'required|number|min:1',
+        'quantidade' => 'required|number|min:1',
+        'linhagem' => 'required|string|min:5',
+        'machos' => 'required|number|min:1',
+        'femeas' => 'required|number|min:1',
+        'total' => 'required|number|min:1',
+        'idade' => 'required|number|min:1',
+        'grupo_animal' => 'required',
+    ];
+
+    public static $messages = [
+        'required' =>  'O campo :attribute é obrigatório.',
+        'string' => 'O campo :attribute deve ser um texto',
+        'number' => 'O campo :attribute deve ser um número',
+        'min' => 'O campo :attribute deve possuir no minimo 5 caracteres',
+    ];
+
     public function modeloAnimal(){
         return $this->belongsTo('App\Models\ModeloAnimal');
     }
