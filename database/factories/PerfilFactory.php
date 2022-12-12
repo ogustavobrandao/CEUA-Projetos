886d6467a3ca189401b02c805a73ed59b1f0aa8d
Edgarvital <edgar.tecnico@hotmail.com>
2022-12-11 21:20:57 -0300

    ajuste no factory de perfil

--- database/factories/PerfilFactory.php
+++ database/factories/PerfilFactory.php
@@ -9,28 +9,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PerfilFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     *     $table->string('grupo_animal');
-    $table->string('linhagem');
-    $table->string('idade');
-    $table->string('peso');
-    $table->string('quantidade');
-    $table->string('machos');
-    $table->string('femeas');
-    $table->string('total');
-    $table->unsignedBigInteger('modelo_animal_id');
-
-    $table->foreign('modelo_animal_id')->references('id')->on('modelo_animals')->onDelete('cascade');
-     */
     public function definition()
     {
         return [
             'grupo_animal' => 'Grupo 5',
             'linhagem' => 'Linhagem',
-            'idade' => '10 dias',
+            'idade' => '10',
+            'periodo' => 'Anos',
             'peso' => '10 kg',
             'quantidade' => '10',
             'machos' => '10',
