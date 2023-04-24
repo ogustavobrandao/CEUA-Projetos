4946ca54585027837114503aecca91d2176a959f
JoseDanielF <danielduartefilho.df@gmail.com>
2023-04-24 20:57:46 -0300

    Tipo de procedência animal adicionado na tela de solicitação

--- resources/views/solicitacao/index.blade.php
+++ resources/views/solicitacao/index.blade.php
@@ -234,7 +234,24 @@
                                     {{$modelo_animal->nome_cientifico}}
                                 </td>
                                 <td>
-                                    {{$modelo_animal->procedencia}}
+                                    @if ($modelo_animal->procedencia == 'animal_comprado')
+                                        Animal Comprado
+                                    @elseif ($modelo_animal->procedencia == 'animal_criacao')
+                                        Animal Criação
+                                    @elseif ($modelo_animal->procedencia == 'animal_doado')
+                                        Animal Doado
+                                    @elseif ($modelo_animal->procedencia == 'animal_silvestre')
+                                        Animal Silvestre
+                                    @elseif ($modelo_animal->procedencia == 'aviario')
+                                        Aviário
+                                    @elseif ($modelo_animal->procedencia == 'bioterio')
+                                        Biotério
+                                    @elseif ($modelo_animal->procedencia == 'fazenda')
+                                        Fazenda
+                                    @elseif ($modelo_animal->procedencia == 'outra_procedencia')
+                                        {{$modelo_animal->outra_procedencia}}}
+                                    @endif
+                                    
                                 </td>
                                 <td>
                                     {{$modelo_animal->perfil->linhagem}}
