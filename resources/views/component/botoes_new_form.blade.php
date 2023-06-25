4e5e52d80dafbece49680b263e746cd3f907b57d
Edgarvital <edgar.tecnico@hotmail.com>
2023-06-25 13:44:47 -0300

    Ajustando o layout da página de solicitação e avaliação.

--- resources/views/component/botoes_new_form.blade.php
+++ resources/views/component/botoes_new_form.blade.php
@@ -5,7 +5,8 @@
         <div class="col-3">
             @if($solicitacao->status == 'avaliado' && $solicitacao->avaliacao->first()->status == 'aprovadaPendencia'
                 && $status == "reprovado" )
-                <a type="button" class="btn btn-danger w-100" onclick="showAvaliacaoIndividual({{$tipo}},{{$solicitacao->avaliacao->first()->id}},{{$id}})"
+                <a type="button" class="btn btn-danger w-100"
+                   onclick="showAvaliacaoIndividual({{$tipo}},{{$solicitacao->avaliacao->first()->id}},{{$id}})"
                 >Pendência</a>
             @endif
         </div>
@@ -13,20 +14,19 @@
             <button type="submit" class="btn btn-success w-100">Salvar</button>
         </div>
     @elseif(Auth::user()->tipo_usuario_id == 2)
-        <div class="col-6">
+        <div class="col-10">
+            <div class="row justify-content-start">
+                <div class="col-3">
+                    <a type="button" class="btn btn-success w-100" onclick="realizarAvaliacaoInd({{$tipo}},{{$avaliacao_id}},{{$id}},'aprovado')">Aprovar</a>
+                </div>
+                <div class="col-3">
+                    @if(!isset($tipo))
+                        {{dd("menssagem!!")}}
+                    @endif
+                    <a type="button" class="btn btn-danger w-100" onclick="showAvaliacaoIndividual({{$tipo}},{{$avaliacao_id}},{{$id}})">Reprovar</a>
+                </div>
 
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
-                    <a type="button" class="btn btn-danger w-100"
-                       onclick="showAvaliacaoIndividual({{$tipo}},{{$avaliacao_id}},{{$id}})">Reprovar</a>
-                </div>
-                <div class="col-4">
-                    <a type="button" class="btn btn-success w-100"
-                       onclick="realizarAvaliacaoInd({{$tipo}},{{$avaliacao_id}},{{$id}},'aprovado')">Aprovar</a>
-                </div>
             </div>
-
         </div>
     @endif
 </div>
