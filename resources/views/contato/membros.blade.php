87a9013160ec89bfe6febb652596146d0fb96733
JoseDanielF <danielduartefilho.df@gmail.com>
2023-06-26 14:59:12 -0300

    Ajuste na tela de membros e sobre na tela inicial

--- resources/views/contato/membros.blade.php
+++ resources/views/contato/membros.blade.php
@@ -10,52 +10,29 @@
     <div class="members-section">
         <h5 class="text-center">A CEUA-UFAPE terá composição multidisciplinar e multiprofissional e será composta por docentes titulares:</h5>
         <br>
-        <div class="member">
-            <h4>DENISE GRANATO CHUNG</h4>
-            <p class="member-role">Coordenadora</p>
-        </div>
 
-        <div class="member">
-            <h4>RACHEL MARIA DE LYRA NEVES</h4>
-            <p class="member-role">Vice coordenadora</p>
-        </div>
+        <h4>Docentes</h4>
+        <br>
 
-        <div class="member">
-            <h4>DENISE FONTANA FIGUEIREDO</h4>
-        </div>
-
-        <div class="member">
-            <h4>OMER CAVALCANTI DE ALMEIDA</h4>
-        </div>
-
-        <div class="member">
-            <h4>SÍLVIA ELAINE RODOLFO DE SÁ LORENA</h4>
-        </div>
-
-        <div class="member">
-            <h4>WALLACE RODRIGUES TELINO JÚNIOR</h4>
-        </div>
-
-        <div class="member">
-            <h4>SAMMARA DRINNY DE SIQUEIRA CORREIA</h4>
-        </div>
+        <ul class="member-list">
+            <li>DENISE GRANATO CHUNG - <strong>Coordernadora</strong></li>
+            <li>RACHEL MARIA DE LYRA NEVES - <strong>Vice coordenadora</strong></li>
+            <li>DENISE FONTANA FIGUEIREDO</li>
+            <li>OMER CAVALCANTI DE ALMEIDA</li>
+            <li>SÍLVIA ELAINE RODOLFO DE SÁ LORENA</li>
+            <li>WALLACE RODRIGUES TELINO JÚNIOR</li>
+            <li>SAMMARA DRINNY DE SIQUEIRA CORREIA</li>
+        </ul>
 
         <h4>Suplentes</h4>
         <br>
 
-        <div class="member">
-            <h4>DANIELA OLIVEIRA</h4>
-        </div>
+        <ul class="member-list">
+            <li>DANIELA OLIVEIRA</li>
+            <li>JORGE EDUARDO CAVALCANTE LUCENA</li>
+            <li>MARCELO MENDONÇA</li>
+        </ul>
 
-        <div class="member">
-            <h4>JORGE EDUARDO CAVALCANTE LUCENA</h4>
-        </div>
-
-        <div class="member">
-            <h4>MARCELO MENDONÇA</h4>
-        </div>
-
-        <!-- Adicione mais membros conforme necessário -->
     </div>
 </div>
 
@@ -74,22 +51,11 @@
         margin-top: 40px;
     }
 
-    .member {
-        padding: 20px;
-        background-color: #f5f5f5;
-        margin-bottom: 20px;
-        border-radius: 8px;
-    }
-
-    .member h4 {
-        margin-bottom: 10px;
-        color: #131833;
-    }
-
-    .member-role {
-        font-weight: bold;
-        color: #555;
-        margin-top: 5px;
+    .member-list {
+        list-style-type: none;
+        margin-left: 0;
+        padding-left: 0;
+        font-size: 1.1em;
     }
 </style>
 
