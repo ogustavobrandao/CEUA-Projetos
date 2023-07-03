0bfe5aa320dfd21566ecc74830bad20ab7b56380
Edgarvital <edgar.tecnico@hotmail.com>
2023-07-03 14:35:08 -0300

    Atualização do template do formulário de procedimento

--- routes/web.php
+++ routes/web.php
@@ -90,7 +90,7 @@ Route::group(['middleware' => ['auth', 'verified', 'checkProprietario']], functi
     Route::post('/solicitacao/criar_resultado', [App\Http\Controllers\SolicitacaoController::class, 'criar_resultado'])->name('solicitacao.resultado.criar');
     Route::post('/solicitacao/criar_operacao', [App\Http\Controllers\SolicitacaoController::class, 'criar_operacao'])->name('solicitacao.operacao.criar');
     Route::post('/solicitacao/criar_solicitacao_fim', [App\Http\Controllers\SolicitacaoController::class, 'criar_solicitacao_fim'])->name('solicitacao.solicitacao_fim.criar');
-    Route::get('/solicitacao/{solicitacao_id}/index', [\App\Http\Controllers\SolicitacaoController::class, 'index_solicitacao'])->name('solicitacao.index');
+    Route::get('/solicitacao/{solicitacao_id}/index', [App\Http\Controllers\SolicitacaoController::class, 'index_solicitacao'])->name('solicitacao.index');
 
     Route::get('/solicitacao/planejamento/index/{modelo_animal_id}', [App\Http\Controllers\SolicitacaoController::class, 'index_planejamento'])->name('solicitacao.planejamento.index');
 
