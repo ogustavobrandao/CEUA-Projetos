eb2cf0135622da498fa08b95267d3ec1028b4767
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-11-06 09:20:34 -0300

    centraliza caixa de seleção

--- resources/views/avaliador/home.blade.php
+++ resources/views/avaliador/home.blade.php
@@ -10,11 +10,9 @@
 
 <div class="container text-center">
     <div class="row justify-content-center mt-5">
-        <div class="col-1">
-        </div>
         <div class="col-10">
             <div class="row justify-content-center">
-                <div class="col-5">
+                <div class="col-5 d-flex justify-content-center">
                     <a href="{{route('solicitacao.avaliador.index')}}">
                         <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable" style="background-color: #143BC2;">
                             <div class="pt-4">
@@ -28,10 +26,6 @@
                 </div>
             </div>
         </div>
-        <div class="col-1">
-
-        </div>
-
     </div>
 </div>
 
