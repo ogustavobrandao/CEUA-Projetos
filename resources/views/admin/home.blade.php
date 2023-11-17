ad2e94ad6bcdd0581acbafe9b0b9b0645ece0ce7
Rener1 <rener.tome123@gmail.com>
2023-11-17 19:41:17 -0300

    Criada nova navbar para diferenciar administradores de usuários comuns. Navbar é definida pelo tipo de usuario selecionado. Adicionado botão de trocar perfil quando o usuário possui mais de 1 tipo de perfil.

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,50 +1,53 @@
 @extends('layouts.app')
 
+@section('navbar-type')
+    @include('layouts.components.navbarAdmin')
+@endsection
+
 @section('content')
-
-<div class="container">
-    <div class="row" style="border-bottom: #131833 2px solid">
-        <span class="titulo text-center">Serviços</span>
+    <div class="container">
+        <div class="row" style="border-bottom: #131833 2px solid">
+            <span class="titulo text-center">Serviços</span>
+        </div>
     </div>
-</div>
 
-<div class="container text-center">
-    <div class="row justify-content-center mt-5">
-        <div class="col-4">
-            <a href="{{ route('instituicao.index') }}">
-                <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
-                     style="background-color: #131833">
-                    <div class="pt-4">
-                        <img class="pb-3" src="../images/vertical_split.svg" height="120px">
-                        <div class="text-center align-middle">
-                            <div class="textoCaixa">Instituições</div>
+    <div class="container text-center">
+        <div class="row justify-content-center mt-5">
+            <div class="col-4">
+                <a href="{{ route('instituicao.index') }}">
+                    <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
+                        style="background-color: #131833">
+                        <div class="pt-4">
+                            <img class="pb-3" src="../images/vertical_split.svg" height="120px">
+                            <div class="text-center align-middle">
+                                <div class="textoCaixa">Instituições</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-4">
-            <a href="{{route('solicitacao.admin.index')}}">
-                <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
-                     style="background-color: #143BC2;">
-                    <div class="pt-4">
-                        <img class="pb-3" src="../images/vertical_split.svg" height="120px">
-                        <div style="color: white" class="textoCaixa w-100">Solicitações</div>
+                </a>
+            </div>
+            <div class="col-4">
+                <a href="{{ route('solicitacao.admin.index') }}">
+                    <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
+                        style="background-color: #143BC2;">
+                        <div class="pt-4">
+                            <img class="pb-3" src="../images/vertical_split.svg" height="120px">
+                            <div style="color: white" class="textoCaixa w-100">Solicitações</div>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-4">
-            <a href="{{route('usuarios.index')}}">
-                <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
-                     style="background-color: #143BC2;">
-                    <div class="pt-4">
-                        <img class="pb-3" src="../images/vertical_split.svg" height="120px">
-                        <div style="color: white" class="textoCaixa w-100">Usuários</div>
+                </a>
+            </div>
+            <div class="col-4">
+                <a href="{{ route('usuarios.index') }}">
+                    <div class="text-center p-5 shadow caixaSelecao justify-content-center clickable"
+                        style="background-color: #143BC2;">
+                        <div class="pt-4">
+                            <img class="pb-3" src="../images/vertical_split.svg" height="120px">
+                            <div style="color: white" class="textoCaixa w-100">Usuários</div>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/solicitante/home.blade.php
+++ resources/views/solicitante/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('navbar-type')
+    @include('layouts.components.navbar')
+@endsection
+
 @section('content')
 
 <div class="container">
