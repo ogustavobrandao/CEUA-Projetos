4df4aa22f5ba20f61494b5171dcc2646d1e21a4e
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2023-12-15 15:54:07 -0300

    A inclusão de avaliadores adicionais, mesmo após a adição de um primeiro avaliador

--- resources/views/admin/solicitacao_atribuir_avaliador.blade.php
+++ resources/views/admin/solicitacao_atribuir_avaliador.blade.php
@@ -39,6 +39,20 @@
                                 <td class="text-center">
                                     <a class="btn btn-group" href="{{ route('solicitacao.admin.visualizar', ['solicitacao_id' => $solicitacao->id]) }}"><i
                                             class="fa-solid fa-up-right-from-square"></i></a>
+
+                                            
+                                    <button class="btn btn-group" type="button" data-toggle="modal"
+                                            data-target="#addAvaliador_{{$solicitacao->id}}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                             viewBox="0 0 24 24" fill="none" stroke="#212529" stroke-width="2"
+                                             stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                            <circle cx="8.5" cy="7" r="4"></circle>
+                                            <line x1="20" y1="8" x2="20" y2="14"></line>
+                                            <line x1="23" y1="11" x2="17" y2="11"></line>
+                                        </svg>
+                                    </button>
+
                                     @if($solicitacao->status == "avaliando")
                                         <button class="btn btn-group" type="button" data-toggle="modal"
                                                 data-target="#removeAvaliador_{{$solicitacao->id}}">
@@ -53,19 +67,7 @@
                                             </a>
                                         </button>
                                     @endif
-                                    @if($solicitacao->status == "nao_avaliado")
-                                        <button class="btn btn-group" type="button" data-toggle="modal"
-                                                data-target="#addAvaliador_{{$solicitacao->id}}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                 viewBox="0 0 24 24" fill="none" stroke="#212529" stroke-width="2"
-                                                 stroke-linecap="round" stroke-linejoin="round">
-                                                <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                                <circle cx="8.5" cy="7" r="4"></circle>
-                                                <line x1="20" y1="8" x2="20" y2="14"></line>
-                                                <line x1="23" y1="11" x2="17" y2="11"></line>
-                                            </svg>
-                                        </button>
-                                    @endif
+
                                 </td>
                             </tr>
                         @endif
